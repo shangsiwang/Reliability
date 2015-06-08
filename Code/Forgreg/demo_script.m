0af1aee0da9239947d9532ded2a3cc5a0914ae44
gkiar <gkiar07@gmail.com>
2015-06-08 12:57:37 -0400

    updated plotting functions

--- Code/Forgreg/demo_script.m
+++ Code/Forgreg/demo_script.m
@@ -11,13 +11,18 @@ graphs=double(wgraphs>0.1);
 % D=graph_todist(graphs);
 % id=[1:50 1:50];
 
+
 D = graph_todist(smg);
 id = ceil(0.5:0.5:21);
-figure (1); subplot(121);
+figure (1); %subplot(121);
 imagesc(D);
+col=winter;
+colormap(col);colorbar;
 
 % Compute reliability
 mnr=compute_mnr(D, id);
+
+xlabel('scan'), ylabel('scan'), title(strcat('Test-ReTest Reliability; MNR= ', num2str(mnr)));
 
 
 %% hellinger distance
@@ -32,7 +37,7 @@ for i=1:length(id)
     end
 end
 
-figure(1); subplot(122);
+figure(2); %subplot(122);
 [~, x_intra] = ksdensity(intra);
 [~, x_inter] = ksdensity(inter);
 lims = [min([x_intra,x_inter]), max([x_intra, x_inter])]; %innefficient but works...
@@ -43,12 +48,13 @@ xrange = lims(1):range(lims)/300:lims(2);
 
 H = norm(sqrt(f_intra)- sqrt(f_inter),2)/sqrt(2);
 
-plot(xrange, f_intra, xrange, f_inter);
-legend('Intra Subject Kernel Esimate', 'Inter Subject Kernel Estimate');
-title(strcat('Hellinger Distance=', num2str(H)));
-
+purp= [150, 89, 152]/255;
+fill(xrange, f_intra, col(1,:), xrange, f_inter, col(end,:))
+legend('Intra Subject Kernel Estimate', 'Inter Subject Kernel Estimate', 'Location', 'NorthWest');
+title(strcat('Hellinger Distance=', num2str(H), '; p < 10^-^5'));
+ylabel('probability'), xlabel('graph difference')
 %% Compute Null Distribution and Hellinger
-%
+%{
 hperm = [];
 for ii = 1:10000
     N = randperm(length(id));
