3885dfa56fc0cdb854c82e17669e06d973b673f7
shangsiwang <swang127@jhu.edu>
2016-01-13 09:51:00 +0800

    update violin plots

--- Code/R/violin_plot.R
+++ Code/R/violin_plot.R
@@ -7,9 +7,10 @@ library(ggplot2)
 nfile<-ncol(data)
 df<-c(nff-frf, fsl-ant,nsc-scr,gsr-ngs)
 gi<-c(rep(1,nfile*32),rep(2,nfile*32),rep(3,nfile*32),rep(4,nfile*32))
-ndf<-c(df,-df)
-df<-cbind(df,2*gi-1,gi)
-df<-rbind(df,cbind(ndf,2*c(gi,gi),rep(5,2*length(gi))))
+# ndf<-c(df,-df)
+# df<-cbind(df,2*gi-1,gi)
+# df<-rbind(df,cbind(ndf,2*c(gi,gi),rep(5,2*length(gi))))
+df<-cbind(df,gi,gi)
 df<-as.data.frame(df)
 
 colnames(df)<-c('df','dec','cg')
@@ -17,36 +18,36 @@ df$dec<-as.factor(df$dec)
 df$cg<-as.factor(df$cg)
 p<-ggplot(data=df,aes(x=dec, y=df,fill=cg))+geom_violin(trim=T)+
   labs(title="Plot of Diffenrence in Reliability",x="Decisions Compared", y = "Difference")+
-  scale_x_discrete(breaks=c("1", "3", "5",'7'),
+  scale_x_discrete(breaks=c("1", "2", "3",'4'),
                    labels=c('nff - frf', 'fsl - ant','nsc - scr','gsr - ngs')) +
-  scale_fill_manual(name="Decisions",
-                    breaks = c("1", "2", "3",'4','5'), 
-                    values=c("#F8766D", "#7CAE00", "#00BFC4", "#C77CFF","grey"),
-                    labels=c('nff - frf', 'fsl - ant','nsc - scr','gsr - ngs','Null Distribution'))
-p 
-
-###############################################
-#Overlay violin plot, not finished
-###############################################
-df<-c(nff-frf, fsl-ant,nsc-scr,gsr-ngs)
-gi<-c(rep(1,nfile*32),rep(2,nfile*32),rep(3,nfile*32),rep(4,nfile*32))
-ndf<-c(df,-df)
-tdf<-c(df,df)
-df<-cbind(tdf,ndf,c(gi,gi),c(gi,gi))
-
-df<-as.data.frame(df)
-colnames(df)<-c('df','ndf','dec','cg')
-df$dec<-as.factor(df$dec)
-df$cg<-as.factor(df$cg)
-
-p<-ggplot(data=df,aes(x=dec, y=df,fill=cg))+
-  geom_violin(trim=T)+ 
   scale_fill_discrete(name="Decisions",
                     breaks = c("1", "2", "3",'4'), 
-                    #values=c("#F8766D", "#7CAE00", "#00BFC4", "#C77CFF"),
                     labels=c('nff - frf', 'fsl - ant','nsc - scr','gsr - ngs'))+
-  geom_violin(aes(y = ndf,fill=cg),trim=T)+
-  labs(title="Plot of Diffenrence in Reliability",x="Decisions Compared", y = "Difference")+
-  scale_x_discrete(breaks=c("1", "2", "3",'4'),
-                   labels=c('nff - frf', 'fsl - ant','nsc - scr','gsr - ngs'))
+  stat_summary(fun.y=mean, geom="point",shape=18, size=4, color="black")
 p 
+
+# ###############################################
+# #Overlay violin plot, not finished
+# ###############################################
+# df<-c(nff-frf, fsl-ant,nsc-scr,gsr-ngs)
+# gi<-c(rep(1,nfile*32),rep(2,nfile*32),rep(3,nfile*32),rep(4,nfile*32))
+# ndf<-c(df,-df)
+# tdf<-c(df,df)
+# df<-cbind(tdf,ndf,c(gi,gi),c(gi,gi))
+# 
+# df<-as.data.frame(df)
+# colnames(df)<-c('df','ndf','dec','cg')
+# df$dec<-as.factor(df$dec)
+# df$cg<-as.factor(df$cg)
+# 
+# p<-ggplot(data=df,aes(x=dec, y=df,fill=cg))+
+#   geom_violin(trim=T)+ 
+#   scale_fill_discrete(name="Decisions",
+#                     breaks = c("1", "2", "3",'4'), 
+#                     #values=c("#F8766D", "#7CAE00", "#00BFC4", "#C77CFF"),
+#                     labels=c('nff - frf', 'fsl - ant','nsc - scr','gsr - ngs'))+
+#   geom_violin(aes(y = ndf,fill=cg),trim=T)+
+#   labs(title="Plot of Diffenrence in Reliability",x="Decisions Compared", y = "Difference")+
+#   scale_x_discrete(breaks=c("1", "2", "3",'4'),
+#                    labels=c('nff - frf', 'fsl - ant','nsc - scr','gsr - ngs'))
+# p 
